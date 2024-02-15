877c25a3b0cb342a64fcf76aeaf77153af580984
DiestroCorp <matias.perez.mcd@gmail.com>
2024-02-15 16:42:36 -0300

    Updated links.

---
+++ php/form-action.php
@@ -0,0 +1,48 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'Exception.php';
+	require 'PHPMailer.php';
+	require 'SMTP.php';
+
+	if(empty($_POST["name"]) or empty($_POST["email"]) or empty($_POST["message"]) or !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+			echo "<script>alert('Error, probá otra vez!');window.location.replace('https://www.diestrocorp.com.ar/portfolio-matias-perez-dev/index.html');</script>";
+			die();
+	}
+
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$message = $_POST["message"];
+
+	$emailBody = "Nombre: ".$name." | E-mail: ".$email." | Mensaje: ".$message;
+
+	//----------------------------Empieza PHPMailer----------------------------------------------------
+	$mail = new PHPMailer(true);
+	try{
+		$mail->setFrom($email, $name.' | Contacto Portfolio Matías Pérez');
+		$mail->addAddress('matias.perez.mcd@gmail.com', 'Matías Pérez Portfolio');
+		$mail->Subject = "Contacto Portfolio Matías Pérez";
+		$mail->Body = $emailBody;		
+   		$mail->CharSet = 'UTF-8';
+		$mail->isSMTP();
+   		$mail->Host = 'diestrocorp.com.ar';//Here goes the server address (example 'smtp.gmail.com').
+   		$mail->SMTPAuth = TRUE;
+  		$mail->SMTPSecure = 'tls';
+   		$mail->Username = 'form@diestrocorp.com.ar';
+   		$mail->Password = '2D5-ZPF8D';
+   		$mail->Port = 587;if($mail->send()){
+			echo "<script>alert('Mensaje enviado!');window.location.replace('https://www.diestrocorp.com.ar/portfolio-matias-perez-dev/gracias.html');</script>";
+			die();
+		}else{
+			echo "<script>alert('Error, probá otra vez!');window.location.replace('https://www.diestrocorp.com.ar/portfolio-matias-perez-dev/index.html');</script>";
+			//print_r(error_get_last());
+			die();
+		}
+	}catch (Exception $e){
+		echo $e->errorMessage();
+	}catch (\Exception $e){
+		echo $e->getMessage();
+	}
+	//----------------------------Termina PHPMailer----------------------------------------------------
+?>
